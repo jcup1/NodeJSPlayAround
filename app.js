5714b35e9b80311e4a15e14bd45aeee137611706
jakubpchmiel@gmail.com <jakubpchmiel@gmail.com>
2018-08-09 09:11:53 +0200

    Extend event emitter

--- app.js
+++ app.js
@@ -1,34 +1,22 @@
 
-const logger = require('./logger')
 const fs = require('fs')
-
 const files = fs.readdirSync('./')
 
 fs.readdir('./', function(error, result) {
 
     if(error) {
-        console.log("Error", error)
+        logger.log(error)
     } else {
-
-        console.log("Result", result)
+        logger.log(result)
     }
 
 })
 
-//class
 const EventEmitter = require('events')
-const emitter = new EventEmitter()
 
-emitter.on('messageLogged', (arg) => {
+const Logger = require('./logger')
+const logger = new Logger()
+
+logger.on('messageLogged', (arg) => {
     console.log('Listener called', arg)
 })
-
-emitter.emit('messageLogged', { id: 1, url: "http://" })
-
-emitter.on('logging', (arg) => {
-    console.log(arg)
-}) 
-
-emitter.emit('logging', {id: 2, message: "Logging"})
-
-console.log(files)
--- logger.js
+++ logger.js
@@ -1,10 +1,18 @@
+const EventEmitter = require('events')
 
 var url = 'http://mylogger.io/log'
 
-function log(message) {
-    //send an http request
+class Logger extends EventEmitter{
 
-    console.log(message)
+    log(message) {
+        //send an http request
+        console.log(message)
+    
+        this.emit('messageLogged', { id: 2, message: "message" })
+    
+    }
+
 }
 
-module.exports.log = log
+
+module.exports = Logger
